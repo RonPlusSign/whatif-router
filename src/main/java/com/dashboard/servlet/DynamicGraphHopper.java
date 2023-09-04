dcd06de38a19ed8cc648d29a8d49f2c60516c782
Andrea Delli <andrea.delli2001@gmail.com>
2023-09-04 17:34:49 +0200

    docs: update copyright comment

--- src/main/java/com/dashboard/servlet/DynamicGraphHopper.java
+++ src/main/java/com/dashboard/servlet/DynamicGraphHopper.java
@@ -1,5 +1,5 @@
 /* WhatIfRouter
- Copyright (C) 2019 DISIT Lab http://www.disit.org - University of Florence
+ Copyright (C) 2023 DISIT Lab http://www.disit.org - University of Florence
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as
  published by the Free Software Foundation, either version 3 of the
