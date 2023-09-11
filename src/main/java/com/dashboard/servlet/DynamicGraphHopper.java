69306f4f8244406e6b7c9ee1a5d30e141a5f69b0
Andrea Delli <andrea.delli2001@gmail.com>
2023-09-11 23:30:56 +0200

    feat: update DynamicGraphHopper to handle different weightings

--- src/main/java/com/dashboard/servlet/DynamicGraphHopper.java
+++ src/main/java/com/dashboard/servlet/DynamicGraphHopper.java
@@ -15,11 +15,13 @@ package com.dashboard.servlet;
 
 import com.graphhopper.GraphHopper;
 import com.graphhopper.config.Profile;
+import com.graphhopper.reader.osm.Pair;
 import com.graphhopper.routing.WeightingFactory;
 import com.graphhopper.routing.ev.*;
 import com.graphhopper.routing.util.EncodingManager;
 import com.graphhopper.routing.weighting.BlockAreaWeighting;
 import com.graphhopper.routing.weighting.FastestWeighting;
+import com.graphhopper.routing.weighting.Weighting;
 import com.graphhopper.storage.GraphEdgeIdFinder;
 import com.graphhopper.util.*;
 import org.apache.commons.io.FileUtils;
@@ -28,43 +30,67 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.*;
 
 
 public class DynamicGraphHopper extends GraphHopper {
-    private GraphEdgeIdFinder.BlockArea blockArea;
-
+    private GraphEdgeIdFinder.BlockArea blockArea;  // Area to avoid during routing
 
     // Mapping between the edge ids and way ids
     private Map<Long, List<Integer>> wayToEdgesMap = new HashMap<>();
 
     // Save mapping between edge id and its way id
-    // NOTE: Edge ids are incremental, starting from 0. It means I can use a simple list in order to store the way ids.
+    // NOTE: Edge ids are incremental, starting from 0. It means I can use a simple list in order to store the mapping between edge (whose is is the position) way id (the value, representing the way id)
     private List<Long> edgeToWayMap = new ArrayList<>();
 
+    // Traffic data of the day and hour of the start of the routing.
+    // RoadElement id => Pair of average traffic density and maximum traffic density for that road element
+    private final Map<String, Pair<Float, Float>> trafficData = new HashMap<>();
 
-    public DynamicGraphHopper() {
+
+    public DynamicGraphHopper(LocalDateTime startTimestamp) {
         super();
+        loadTrafficData(startTimestamp);
+        if (wayToEdgesMap.isEmpty() || edgeToWayMap.isEmpty()) {
+            // Check if the files containing the mappings between the way and the edges exist, and if so, deserialize them
+            if (new File(getGraphHopperLocation() + "/wayToEdgesMap.json").exists() && new File(getGraphHopperLocation() + "/edgeToWayMap.json").exists())
+                deserializeMappings();
+        }
     }
 
     // Override the createWeighting method of the GraphHopper class to enable BlockAreaWeighting
     @Override
     protected WeightingFactory createWeightingFactory() {
+        // Get encoded values for the vehicle
+        EncodingManager em = this.getEncodingManager();
+        BooleanEncodedValue accessEnc = em.getBooleanEncodedValue(VehicleAccess.key(this.getProfiles().get(0).getVehicle()));
+        DecimalEncodedValue speedEnc = em.getDecimalEncodedValue(VehicleSpeed.key(this.getProfiles().get(0).getVehicle()));
 
+        // Get the weighting in use
         String weighting = this.getProfiles().get(0).getWeighting();
+        WeightingFactory result;
 
-        if ("block_area".equalsIgnoreCase(weighting)) {
-            // Get encoded values for the vehicle
-            EncodingManager em = this.getEncodingManager();
-            BooleanEncodedValue accessEnc = em.getBooleanEncodedValue(VehicleAccess.key(this.getProfiles().get(0).getVehicle()));
-            DecimalEncodedValue speedEnc = em.getDecimalEncodedValue(VehicleSpeed.key(this.getProfiles().get(0).getVehicle()));
+        if (weighting.equals("fastest_with_traffic")) {
+            if (wayToEdgesMap.isEmpty() || edgeToWayMap.isEmpty()) {
+                result = (Profile profile, PMap hints, boolean disableTurnCosts) -> new FastestWeighting(accessEnc, speedEnc);
+            }
+            else {
+                result = (Profile profile, PMap hints, boolean disableTurnCosts) -> new FastestWeightingWithTraffic(trafficData, accessEnc, speedEnc, wayToEdgesMap, edgeToWayMap);
+            }
+        }
+        // Other default weightings, like "shortest", "short_fastest", etc. See https://github.com/graphhopper/graphhopper/blob/master/docs/core/profiles.md
+        else result = super.createWeightingFactory();
 
-            // Create a new WeightingFactory, with the createWeighting method that returns a BlockAreaWeighting
-            return (Profile profile, PMap hints, boolean disableTurnCosts) -> new BlockAreaWeighting(new FastestWeighting(accessEnc, speedEnc), blockArea);
+        // Add the blockArea to the weighting
+        if (blockArea != null) {
+            // Create a new WeightingFactory, with the createWeighting method that returns a BlockAreaWeighting if a BlockArea is set and uses the "result" weighting otherwise
+            return (Profile profile, PMap hints, boolean disableTurnCosts) -> {
+                Weighting w = result.createWeighting(profile, hints, disableTurnCosts);
+                return new BlockAreaWeighting(w, blockArea);
+            };
         }
-        else {
-            return super.createWeightingFactory();
-        }
+        else return result;
     }
 
     public void setBlockArea(GraphEdgeIdFinder.BlockArea ba) {
@@ -90,45 +116,33 @@ public class DynamicGraphHopper extends GraphHopper {
                 reader.readGraph();
                 wayToEdgesMap = reader.getWayToEdgesMap();
                 edgeToWayMap = reader.getEdgeToWayMap();
-
-                System.out.println("wayToEdgesMap: " + wayToEdgesMap.size());
-                System.out.println("edgeToWayMap: " + edgeToWayMap.size());
-
                 serializeMappings();    // Save the mappings between the way and the edges
             } catch (IOException e) {
                 System.out.println("Error while reading the graph");
             }
         }
-        else {
-            deserializeMappings();  // Load the mappings between the way and the edges
-
-            System.out.println("Deserialized wayToEdgesMap: " + wayToEdgesMap.size());
-            System.out.println("Deserialized edgeToWayMap: " + edgeToWayMap.size());
-        }
-
-        System.out.println("Graph loaded successfully");
-
+        else deserializeMappings();  // Load the mappings between the way and the edges
         return this;
     }
 
-    /** Serialize the mappings between the way and the edges that belong to it, saving them in a file */
+    /**
+     * Serialize the mappings between the way and the edges that belong to it, saving them in a file
+     */
     private void serializeMappings() {
-        // Serialize the wayToEdgesMap
         try {
+            // Serialize the wayToEdgesMap
             FileUtils.writeLines(new File(getGraphHopperLocation() + "/wayToEdgesMap.json"), Collections.singleton(new JSONObject(wayToEdgesMap)));
-        } catch (IOException e) {
-            System.out.println("Error while serializing the wayToEdgesMap");
-        }
 
-        // Serialize the edgeToWayMap
-        try {
+            // Serialize the edgeToWayMap
             FileUtils.writeLines(new File(getGraphHopperLocation() + "/edgeToWayMap.json"), Collections.singleton(new JSONArray(edgeToWayMap)));
         } catch (IOException e) {
-            System.out.println("Error while serializing the edgeToWayMap");
+            System.out.println("Error while serializing the mappings");
         }
     }
 
-    /** Read the mappings between the way and the edges that belong to it, saved in a file and deserialize them (recreate the wayToEdgesMap and the edgeToWayMap) */
+    /**
+     * Read the mappings between the way and the edges that belong to it, saved in a file and deserialize them (recreate the wayToEdgesMap and the edgeToWayMap)
+     */
     private void deserializeMappings() {
         // Deserialize the wayToEdgesMap
         try {
@@ -158,6 +172,37 @@ public class DynamicGraphHopper extends GraphHopper {
             }
         } catch (IOException e) {
             System.out.println("Error while deserializing the edgeToWayMap");
+        }
+    }
+
+    /**
+     * Initialize the traffic data map of the day and hour of the start of the routing.
+     * <p>
+     * This method reads a JSON file from the "typical_ttt" folder, named D_HH.json, where D is the day of the week (0 = Monday, 6 = Sunday) and HH is the hour of the day (00, 01, ..., 23).
+     * The JSON file contains a map of the road elements and their average and maximum traffic density.
+     * Format: { "roadElementId": { "ttt": float, "max": float }, ... }
+     *
+     * @param startTimestamp the timestamp of the start of the routing, which is used to get the day and hour of the routing
+     */
+    private void loadTrafficData(LocalDateTime startTimestamp) {
+        String day = String.valueOf(startTimestamp.getDayOfWeek().getValue() - 1); // Day of the week (0 = Monday, 6 = Sunday)
+        String hour = String.format("%02d", startTimestamp.getHour());  // Hour of the day (00, 01, ..., 23)
+
+        // Read the JSON file
+        try {
+            String json = FileUtils.readFileToString(new File("typical_ttt/" + day + "_" + hour + ".json"));
+            JSONObject jsonObject = new JSONObject(json);
+            jsonObject.keys().forEachRemaining(keyStr -> {
+                // For each road element, get the average and maximum traffic density
+                JSONObject roadElement = jsonObject.getJSONObject(keyStr);
+                float ttt = roadElement.getFloat("ttt");
+                float max = roadElement.getFloat("max");
+
+                // Add the road element and its traffic data to the traffic data map
+                trafficData.put(keyStr, new Pair<>(ttt, max));
+            });
+        } catch (IOException e) {
+            System.out.println("Error while reading the traffic data");
         }
     }
 }
